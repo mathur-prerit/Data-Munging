2bfe3d651d8e5818d4b8e218a24cd47fe4238577
Prerit Mathur <mathur.prerit@gmail.com>
2019-12-07 17:22:57 +0530

    Created a common function to read the file for both weather and football

---
+++ Common.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const csv = require ("fast-csv");
+
+let data = []
+let resultObject = {key:'',x:0,y:0,diff:0};
+class readingData {
+       static readFile(path){
+           return new Promise((resolve,reject) => {
+            fs.createReadStream(path)
+  .pipe(csv.parse({ headers: false })) 
+  .on('data', row => {
+            data.push(row)
+        }).on('end', () => {
+            if (data){
+            resolve(data)
+            }
+            reject('error');
+        })
+    })
+    };
+
+
+    }
+
+//}
+//const hello = new readingData();
+//  readingData.readFile('./weather.dat').then(()=>{
+//      console.log(data);
+//  });
+//console.log(hello.readFile('./football.dat'));
+
+module.exports = { readingData }
